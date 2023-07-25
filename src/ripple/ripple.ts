fefb6f92c1b35f37dd63ac3ef4e0324fa2490089
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2023-07-24 23:15:46 -0600

    feat(ripple): Added the color, opacity and duration property

--- src/ripple/ripple.ts
+++ src/ripple/ripple.ts
@@ -1,14 +1,40 @@
 import { LitElement, PropertyValueMap, css, html, unsafeCSS } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 
 import styles from './ripple.css?inline'
+
+import { ColorNameMap } from '../declarations';
+
+import { getColor } from '../utils';
 
 @customElement('hwc-ripple')
 export class Ripple extends LitElement {
   static styles = css`${unsafeCSS(styles)}`;
 
+  @property({ type: String }) color!: string
+
+  @property({ type: String }) duration = '700ms'
+
+  @property({ type: Number }) opacity = 0.35
+
   protected firstUpdated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
     this.addEventListener('click', this.createRipple);
+  }
+
+  protected updated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+    if (_changedProperties.has('duration')) {
+      this.style.setProperty('--hwc-ripple-animation-duration', this.duration)
+    }
+
+    if (_changedProperties.has('opacity')) {
+      this.style.setProperty('--hwc-ripple-opacity', this.opacity.toString())
+    }
+
+    if (_changedProperties.has('opacity')) {
+      const color = getColor(this.color as ColorNameMap) || this.color
+
+      this.style.setProperty('--hwc-ripple-bg', color || null)
+    }
   }
 
   private createRipple(event: any) {
