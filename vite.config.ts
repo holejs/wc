94fe2eac0c9f0906b169751730023a3263f1039b
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2023-07-27 15:58:18 -0600

    chore: initialize configuration environments

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: [
+        path.resolve(__dirname, './src/index.ts'),
+        path.resolve(__dirname, './src/button/button.ts'),
+        path.resolve(__dirname, './src/ripple/ripple.ts')
+      ],
+      formats: ['es']
+    },
+    rollupOptions: {}
+  },
   test: {
     globals: true,
     environment: 'jsdom'
-  }
+  },
+  plugins: [
+    dts({ outDir: './dist/types' })
+  ]
 })
