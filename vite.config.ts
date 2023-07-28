6e184831e9e20f17ecc531e368565960d7ffe6ce
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2023-07-28 13:13:54 -0600

    chore: Assets generation was configured

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,38 @@
 /// <reference types="vitest" />
 
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true,
     lib: {
       entry: [
-        path.resolve(__dirname, './src/index.ts'),
         path.resolve(__dirname, './src/button/button.ts'),
-        path.resolve(__dirname, './src/ripple/ripple.ts')
+        path.resolve(__dirname, './src/ripple/ripple.ts'),
+        path.resolve(__dirname, './src/index.ts')
       ],
       formats: ['es']
-    },
-    rollupOptions: {}
+    }
   },
   test: {
     globals: true,
     environment: 'jsdom'
   },
   plugins: [
-    dts({ outDir: './dist/types' })
+    // Generate d.ts.
+    dts({ outDir: './dist/types' }),
+
+    // Generate static files.
+    viteStaticCopy({
+      targets: [
+        {
+          src: './src/assets/**/*.css',
+          dest: './assets'
+        }
+      ]
+    })
   ]
 })
