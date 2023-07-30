ae9bad99b8d1637ccd35360a82cbfa7a1f402c62
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2023-07-30 16:28:49 -0600

    feat(chip): Added the "rounded" property

--- src/chip/chip.ts
+++ src/chip/chip.ts
@@ -2,6 +2,7 @@ import { CSSResultGroup, LitElement, PropertyValueMap, css, html, unsafeCSS } fr
 import { customElement, property, query } from 'lit/decorators.js'
 
 import styles from './chip.css?inline'
+
 import { isValidColorFormat } from '../utils'
 
 @customElement('hwc-chip')
@@ -12,13 +13,13 @@ export default class Chip extends LitElement {
 
   @property({ type: String }) color: string = 'blue-lighten-1'
 
+  @property({ type: Boolean }) rounded: boolean = false
+
   protected updated (_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
     if (_changedProperties.has('color')) {
       const color = isValidColorFormat(this.color) ? `var(--hwc-${this.color})` : this.color
 
       this.style.setProperty('--hwc-chip-color', color)
-
-      console.log(getComputedStyle(this.$chip).backgroundColor)
     }
   }
 
