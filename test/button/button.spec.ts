7c460d71afd71c21616676018bc8dc6fd85ba805
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2023-08-09 00:05:03 -0600

    test(button): add unit test for default button rendering

--- test/button/button.spec.ts
+++ test/button/button.spec.ts
@@ -1,16 +1,29 @@
-import { test, expect, describe } from 'vitest'
-import { html } from 'lit'
+/* eslint-disable no-undef */
 
-import { _query, _render } from '../utils'
+import { test, expect, describe } from 'vitest'
+import { fixture } from '@open-wc/testing'
+import { html } from 'lit'
 
 import '../../src/button/button'
 
+type HWCButton = HTMLElementTagNameMap['hwc-button']
+
 describe('Button', () => {
   test('Render default button', async () => {
-    _render(html`<hwc-button>Click me!</hwc-button>`)
+    const $hwcbutton = await fixture<HWCButton>(
+      html`<hwc-button>Click me!</hwc-button>`
+    )
 
-    const $button = _query('hwc-button')
+    const $button = $hwcbutton.shadowRoot?.querySelector('button')
 
-    expect($button?.textContent).toContain('Click me!')
+    expect($button).not.toBe(null)
+    expect($hwcbutton?.textContent).toContain('Click me!')
+    expect($hwcbutton.type).toBe('button')
+    expect($button?.type).toBe('button')
+    expect($hwcbutton.lowercase).toBe(false)
+    expect($hwcbutton.uppercase).toBe(false)
+    expect($hwcbutton.capitalize).toBe(false)
+    expect($hwcbutton.rounded).toBe(false)
+    expect($hwcbutton.fullwidth).toBe(false)
   })
 })
