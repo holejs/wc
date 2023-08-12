5a28a2c44fe90003ae30d00cfd5d6fa45f59757f
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2023-08-12 00:01:55 -0600

    feat(checkbox): the component was attached to the form

--- src/checkbox/checkbox.ts
+++ src/checkbox/checkbox.ts
@@ -1,5 +1,5 @@
 import { CSSResultGroup, LitElement, PropertyValueMap, css, html, unsafeCSS } from 'lit'
-import { customElement, property, state } from 'lit/decorators.js'
+import { customElement, property, query, state } from 'lit/decorators.js'
 
 import styles from './checkbox.css?inline'
 
@@ -16,15 +16,31 @@ declare global {
 export default class Checkbox extends LitElement {
   static styles?: CSSResultGroup | undefined = css`${unsafeCSS(styles)}`
 
+  @query('input') $input!: HTMLInputElement
+
+  readonly internals = this.attachInternals()
+
+  // More information: https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/attachInternals#examples
+  static formAssociated = true
+
   @property({ type: String }) color!: string
 
   @property({ type: String }) name!: string
+
+  /**
+   * For more information visit: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox#additional_attributes
+   */
+  @property({ type: String }) value: string = 'on'
 
   @property({ type: Boolean }) checked = false
 
   @property({ type: Boolean }) disabled = false
 
   @state() uniqueId = `checkbox-${generateHash()}`
+
+  protected firstUpdated (_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+    this._setValue(this.value)
+  }
 
   protected updated (_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
     if (_changedProperties.has('color')) {
@@ -34,6 +50,22 @@ export default class Checkbox extends LitElement {
     }
   }
 
+  private _setValue (value: string): void {
+    this.value = value
+
+    this.$input.value = value
+
+    this.internals.setFormValue(this.checked ? value : null)
+  }
+
+  private _handleChange (_ev: Event) {
+    const $input = this.$input
+
+    this.checked = $input.checked
+
+    this._setValue(this.$input.value)
+  }
+
   protected render (): unknown {
     return html`
       <div class="checkbox">
@@ -41,8 +73,10 @@ export default class Checkbox extends LitElement {
           id=${this.uniqueId}
           type="checkbox"
           name=${this.name}
+          .value=${this.value}
           ?checked=${this.checked}
           ?disabled=${this.disabled}
+          @change=${this._handleChange}
         >
         <label class="checkbox__label" for=${this.uniqueId}>
           <slot></slot>
