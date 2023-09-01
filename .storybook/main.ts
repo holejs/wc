98bc675bf198450cbd8f1ce1743268f597832399
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2023-08-31 21:08:44 -0600

    chore: updated storybook dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,7 +8,8 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
   ],
   framework: {
     name: "@storybook/web-components-vite",
