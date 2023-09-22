6d092c3e9f349514cbf7476238b36169a33f092d
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2023-09-22 16:29:58 -0600

    chore(select): initialize design

--- src/select/select.stories.ts
+++ src/select/select.stories.ts
@@ -12,7 +12,47 @@ const meta = {
   title: 'Example/Selects',
   tags: ['autodocs'],
   render: (_args: any) => html`
-    <hwc-select></hwc-select>
+    <div>
+      <style>
+        .container__option {
+          display: flex;
+          align-items: center;
+        }
+        .container__option span {
+          height: 15px;
+          width: 15px;
+          border-radius: 3px;
+        }
+        .container__option p {
+          margin: 0;
+          margin-left: 10px;
+        }
+      </style>
+      <div class="row justify-content-center">
+        <div class="col-8 col-sm-10 col-md-8">
+          <hwc-select>
+            <hwc-option>
+              <div class="container__option">
+                <span style="background: #963ec5;"></span>
+                <p>Chinese Violet</p>
+              </div>
+            </hwc-option>
+            <hwc-option>
+              <div class="container__option">
+                <span style="background: #493ec5;"></span>
+                <p>Doguer Blue</p>
+              </div>
+            </hwc-option>
+            <hwc-option>
+              <div class="container__option">
+                <span style="background: #3ec5c5;"></span>
+                <p>Turquoise</p>
+              </div>
+            </hwc-option>
+          </hwc-select>
+        </div>
+      </div>
+    </div>
   `,
   argTypes: {}
 }
--- src/select/select.ts
+++ src/select/select.ts
@@ -1,5 +1,7 @@
-import { CSSResultGroup, LitElement, css, html, unsafeCSS } from 'lit'
-import { customElement } from 'lit/decorators.js'
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
+import { CSSResultGroup, LitElement, PropertyValueMap, css, html, unsafeCSS } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
 
 import styles from './select.css?inline'
 
@@ -7,18 +9,64 @@ declare global {
   // eslint-disable-next-line no-unused-vars
   interface HTMLElementTagNameMap {
     'hwc-select': HWCSelect;
+    'hwc-option': HWCOption;
   }
 }
 
 @customElement('hwc-select')
-export default class Select extends LitElement {
+export class HWCSelect extends LitElement {
   static styles?: CSSResultGroup | undefined = css`${unsafeCSS(styles)}`
+
+  @property({ attribute: false }) selectedOption!: Node
+
+  protected firstUpdated (_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+    this.$options.forEach((option) => {
+      option.addEventListener('click', () => {
+        this.$options.forEach((option) => option.removeAttribute('selected'))
+        option.setAttribute('selected', '')
+
+        console.log('Selected!')
+        this.selectedOption = option.cloneNode(true)
+      })
+    })
+  }
+
+  get $options () {
+    return this?.querySelectorAll('hwc-option')
+  }
 
   protected render (): unknown {
     return html`
-      <div>Select!</div>
+      <div class="select">
+        <div class="select__wrapper">
+          <div class="select__content">
+            <button class="select__control">
+              ${this.selectedOption}
+            </button>
+          </div>
+
+          <div class="select__suggestions">
+            <ul class="select-suggestions__wrapper">
+              <slot></slot>
+            </ul>
+          </div>
+        </div>
+      </div>
     `
   }
 }
 
-export class HWCSelect extends Select {}
+@customElement('hwc-option')
+export class HWCOption extends LitElement {
+  static styles?: CSSResultGroup | undefined = css`${unsafeCSS(styles)}`
+
+  protected render (): unknown {
+    return html`
+      <li class="select-suggestions__item">
+        <button class="select-suggestion__button">
+          <slot></slot>
+        </button>
+      </li>
+    `
+  }
+}
