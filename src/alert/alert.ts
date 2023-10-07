b73273040b73d7f642819e48adbeea1092b89d0d
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2023-10-07 08:30:32 -0600

    refactor(alert): Some types were removed

--- src/alert/alert.ts
+++ src/alert/alert.ts
@@ -1,20 +1,12 @@
-import {
-  PropertyValueMap,
-  CSSResultGroup,
-  TemplateResult,
-  LitElement,
-  unsafeCSS,
-  html,
-  css
-} from 'lit'
+import { PropertyValueMap, LitElement, unsafeCSS, html, css } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import { when } from 'lit/directives/when.js'
 
 import styles from './alert.css?inline'
 
-import { isValidColorFormat } from '../utils/isValidColorFormat.js'
-
 import '../button/button.js'
+
+import { isValidColorFormat } from '../utils/isValidColorFormat.js'
 
 declare global {
   // eslint-disable-next-line no-unused-vars
@@ -29,18 +21,20 @@ export type AlertType = 'info' | 'success' | 'warning' | 'error'
 
 @customElement('hwc-alert')
 export class HWCAlert extends LitElement {
-  static styles?: CSSResultGroup | undefined = css`${unsafeCSS(styles)}`
+  static styles = css`${unsafeCSS(styles)}`
 
   @property({ type: String }) appearance: AlertAppearance = 'filled'
 
   @property({ type: String, reflect: true }) type!: AlertType
 
-  @property({ type: Boolean }) dismissible: boolean = false
+  @property({ type: String, reflect: true }) role = 'alert'
+
+  @property({ type: Boolean }) dismissible = false
 
   @property({ type: String }) color!: string
 
-  protected updated (_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
-    if (_changedProperties.has('color')) {
+  protected updated (changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+    if (changedProperties.has('color')) {
       const color = isValidColorFormat(this.color) ? `var(--hwc-${this.color})` : this.color
 
       this.style.setProperty('--hwc-alert-color', color)
@@ -63,25 +57,53 @@ export class HWCAlert extends LitElement {
     this.remove()
   }
 
-  private iconTmpl (type: AlertType) {
-    const icon: Record<AlertType, TemplateResult<1>> = {
+  private _iconTemplate (type: AlertType): unknown {
+    const icon: Record<AlertType, unknown> = {
       info: html`
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-info-circle-fill"
+          viewBox="0 0 16 16"
+        >
           <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
         </svg>
       `,
       success: html`
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-check-circle-fill"
+          viewBox="0 0 16 16"
+        >
           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
         </svg>
       `,
       warning: html`
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-exclamation-triangle-fill"
+          viewBox="0 0 16 16"
+        >
           <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </svg>
       `,
       error: html`
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-octagon-fill" viewBox="0 0 16 16">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-exclamation-octagon-fill"
+          viewBox="0 0 16 16"
+        >
           <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </svg>
       `
@@ -94,31 +116,47 @@ export class HWCAlert extends LitElement {
     return html`
       <div class="alert">
         <div class="alert__wrapper">
-          ${when(
-            this.type,
-            () => html`
-              <span class="alert__icon">
-                ${this.iconTmpl(this.type)}
-              </span>
-            `
-          )}
+          ${
+            when(
+              this.type,
+              () => html`
+                <span class="alert__icon">
+                  ${this._iconTemplate(this.type)}
+                </span>
+              `
+            )
+          }
 
           <div class="alert__content">
             <slot></slot>
           </div>
 
-          ${when(
-            this.dismissible,
-            () => html`
-              <div class="alert__actions">
-                <hwc-button @click=${() => this.close()} class="alert-button__close" appearance="icon">
-                  <svg class="alert-icon__close" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
-                  </svg>
-                </hwc-button>
-              </div>
-            `
-          )}
+          ${
+            when(
+              this.dismissible,
+              () => html`
+                <div class="alert__actions">
+                  <hwc-button
+                    appearance="icon"
+                    class="alert-button__close"
+                    @click=${() => this.close()}
+                  >
+                    <svg
+                      class="alert-icon__close"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-x-circle-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+                    </svg>
+                  </hwc-button>
+                </div>
+              `
+            )
+          }
         </div>
       </div>
     `
