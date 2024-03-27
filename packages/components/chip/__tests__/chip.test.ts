097eee85962fb9028754e8eac735c8b685d6dbac
Ivan Guevara <ivanzaldivar16@gmail.com>
2024-03-27 16:49:01 -0600

    test: Add unit tests for HWCChip

--- packages/components/chip/__tests__/chip.test.ts
+++ packages/components/chip/__tests__/chip.test.ts
@@ -1,7 +1,42 @@
-'use strict';
+import { fixture, html, expect } from '@open-wc/testing'
 
-const chip = require('..');
-const assert = require('assert').strict;
+import type { HWCButton } from '@holejs/wc-button'
+import type { HWCChip } from '../dist/index.js'
+import '../dist/index.js'
 
-assert.strictEqual(chip(), 'Hello from chip');
-console.info('chip tests passed');
+describe('<hwc-chip>', () => {
+  it('should render a chip', async () => {
+    const $chip = await fixture<HWCChip>(html`<hwc-chip>Guns N' Roses</hwc-chip>`)
+
+    expect($chip.appearance).to.be.equal('filled')
+    expect($chip.size).to.be.equal('regular')
+    expect($chip.color).to.be.undefined
+    expect($chip.rounded).to.be.false
+    expect($chip.closable).to.be.false
+    expect($chip.shadowRoot?.querySelector('.chip')).to.exist
+  })
+
+  describe('when chip is closable', () => {
+    it('should render a chip when is closable', async () => {
+      const $chip = await fixture<HWCChip>(html`<hwc-chip closable>Guns N' Roses</hwc-chip>`)
+  
+      const $button = $chip.shadowRoot?.querySelector<HWCButton>('.chip-button__closable')
+  
+      expect($chip.closable).to.be.true
+      expect($button).to.exist
+  
+      $button?.click()
+  
+      expect(document.body.contains($chip)).to.be.false
+    })
+
+    it('should not close chip when event is cancelled', async () => {
+      const $chip = await fixture<HWCChip>(html`<hwc-chip closable>Guns N' Roses</hwc-chip>`)
+  
+      $chip.addEventListener('close', e => e.preventDefault())
+      $chip.close()
+  
+      expect(document.body.contains($chip)).to.be.true
+    })
+  })
+})
