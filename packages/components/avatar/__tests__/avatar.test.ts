314a72a7f67a3a09c4ef95bd8ccd205bc1408c40
Ivan Guevara <ivanzaldivar16@gmail.com>
2024-03-27 11:17:46 -0600

    test: Add unit tests for HWCAvatar

--- packages/components/avatar/__tests__/avatar.test.ts
+++ packages/components/avatar/__tests__/avatar.test.ts
@@ -1,7 +1,40 @@
-'use strict';
+import { fixture, html, expect } from "@open-wc/testing"
 
-const avatar = require('..');
-const assert = require('assert').strict;
+import "../dist/index.js"
 
-assert.strictEqual(avatar(), 'Hello from avatar');
-console.info('avatar tests passed');
+const _stripExpressionComments = (html: string) =>
+  html.replace(/<!--\?lit\$[0-9]+\$-->|<!--\??-->/g, '');
+
+const _removeWhitespace = (text: string | undefined | null) => (text || '').replace(/\s/g, '')
+
+describe('<hwc-avatar>', () => {
+  it('should renders an avatar with initials when no src is provided', async () => {
+    const $avatar = await fixture(html`
+      <hwc-avatar name="Ivan Guevara"></hwc-avatar>
+    `)
+
+    expect($avatar.shadowRoot?.querySelector('.avatar__text')).to.be.ok
+    expect(
+      _removeWhitespace(
+        _stripExpressionComments($avatar.shadowRoot?.querySelector('.avatar__text')?.textContent || '')
+      )
+    ).to.be.equal('IG')
+  })
+
+  it('should renders an avatar with image when src is provided', async () => {
+    const $avatar = await fixture(html`
+      <hwc-avatar name="Ivan Guevara" src="https://avatars.githubusercontent.com/u/2918581"></hwc-avatar>
+    `)
+
+    expect($avatar.shadowRoot?.querySelector('.avatar__image')).to.be.ok
+  })
+
+  it('should sets correct color to the avatar', async () => {
+    const $avatar = await fixture(html`
+      <hwc-avatar name="Ivan Guevara" color="#27ae60"></hwc-avatar>
+    `)
+
+    expect($avatar.shadowRoot?.querySelector('.avatar__text')).to.be.ok
+    expect(getComputedStyle($avatar.shadowRoot?.querySelector('.avatar') as Element).backgroundColor).to.be.equal('rgb(39, 174, 96)')
+  })
+})
