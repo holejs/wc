95f743495c83706cf252ffac882738f025cf99cf
Ivan Guevara <ivanzaldivar16@gmail.com>
2024-03-28 20:59:13 -0600

    test: Add tests for default values in HWCSelect

--- packages/components/select/__tests__/select.test.ts
+++ packages/components/select/__tests__/select.test.ts
@@ -1,7 +1,21 @@
-'use strict';
+import { fixture, html, expect } from '@open-wc/testing'
 
-const select = require('..');
-const assert = require('assert').strict;
+import type { HWCSelect } from '../dist/index.js'
+import '../dist/index.js'
 
-assert.strictEqual(select(), 'Hello from select');
-console.info('select tests passed');
+describe('<hwc-select>', () => {
+  describe('when default values', () => {
+    it('should render the select with the default values', async () => {
+      const $select = await fixture<HWCSelect>(html`<hwc-select></hwc-select>`)
+
+      expect($select).to.be.accessible()
+      expect($select.label).to.be.undefined
+      expect($select.hint).to.be.undefined
+      expect($select.color).to.be.undefined
+      expect($select.multiple).to.be.false
+      expect($select.value).to.be.null
+      expect($select.disabled).to.be.false
+      expect($select.shadowRoot?.querySelector('.select')).to.be.exist
+    })
+  })
+})
