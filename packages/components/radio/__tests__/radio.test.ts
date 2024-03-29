c8b2452c587d346b366a227743981431c32db341
Ivan Guevara <ivanzaldivar16@gmail.com>
2024-03-28 19:42:49 -0600

    test: Added tests for the HWCRadio

--- packages/components/radio/__tests__/radio.test.ts
+++ packages/components/radio/__tests__/radio.test.ts
@@ -1,7 +1,50 @@
-'use strict';
+import { fixture, html, expect, elementUpdated } from '@open-wc/testing'
 
-const radio = require('..');
-const assert = require('assert').strict;
+import type { HWCRadio } from '../dist/index.js'
+import '../dist/index.js'
 
-assert.strictEqual(radio(), 'Hello from radio');
-console.info('radio tests passed');
+describe('<hwc-radio>', () => {
+  describe('when default values', () => {
+    it('should render the radio with any attributes', async () => {
+      const $radio = await fixture<HWCRadio>(html`<hwc-radio></hwc-radio>`)
+
+      expect($radio).to.be.accessible()
+      expect($radio.value).to.be.equal('on')
+      expect($radio.role).to.be.equal('radio')
+      expect($radio.color).to.be.undefined
+      expect($radio.checked).to.be.false
+      expect($radio.shadowRoot?.querySelector('.radio__wrapper')).to.be.ok
+    })
+  })
+
+  describe('when include validations', () => {
+    it('should be invalid when not checked & default error message', async () => {
+      const $form = await fixture<HTMLFormElement>(html`
+        <form>
+          <hwc-radio
+            name="pet"
+            value="dog"
+            rules="required"
+          >Dog. 🐶</hwc-radio>
+        </form>
+      `)
+
+      const $radio = $form.querySelector('hwc-radio') as HWCRadio
+
+      expect($radio).to.be.accessible()
+      expect($radio.validationMessage).to.be.equal('Please select one of these options.')
+      expect($form.checkValidity()).to.be.false
+
+      $radio.checked = true
+
+      await elementUpdated($radio)
+
+      expect($radio.validationMessage).to.be.null
+      expect($form.checkValidity()).to.be.true
+
+      const formData = new FormData($form)
+
+      expect(formData.get('pet')).to.be.equal('dog')
+    })
+  })
+})
