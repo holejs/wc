eebdd3365addae943eebb7142b06a97983c50c4e
Ivan Guevara <ivanzaldivar16@gmail.com>
2024-08-05 21:30:42 -0600

    docs: add JSDoc comments for validation properties and methods

--- src/internals/input-field.ts
+++ src/internals/input-field.ts
@@ -25,18 +25,9 @@ export const INPUT_FIELD_ATTR_ERROR_MSG = {
 } as const
 
 /**
- * Class responsible for adding validations to the input element.
- *
- * **Note**: When you extend this class and you need to use [Lifecycles](https://lit.dev/docs/components/lifecycle/)
- * you need to invoke the parent class.
- *
- * @example
- *
- * ```ts
- * class MyInput extends InputField {
- *   // Your code here.
- * }
- * ```
+ * The `InputField` class is an abstract class that serves as the base class
+ * for all input field components. It provides a set of properties and methods
+ * that are common to all input field components.
  */
 export abstract class InputField<T> extends LitElement {
   @query('input')
@@ -85,6 +76,7 @@ export abstract class InputField<T> extends LitElement {
    * **NOTE**: The rules are separated by a pipe character `|`. It's important
    * the `pattern` must be defined last, otherwise it will not work correctly.
    *
+   * @deprecated
    * @example
    * ```html
    * <hwc-text-field rules="required|minlength:3|maxlength:15"></hwc-text-field>
@@ -97,56 +89,130 @@ export abstract class InputField<T> extends LitElement {
   @property()
     rules = ''
 
+  /**
+   * Indicates whether the input field is required.
+   *
+   * @default false
+   */
   @property()
     required = false
 
+  /**
+   * The error message displayed when the input field is required but not filled.
+   *
+   * @default 'This field is required'
+   */
   @property({ attribute: INPUT_FIELD_ATTR_ERROR_MSG.Required })
     errorMessageRequired = 'This field is required'
 
-  @property({ type: Number })
+  /**
+   * The minimum number of characters allowed in the input field.
+   *
+   * @default null
+   */
+  @property({ type: Number, attribute: 'minlength' })
     minLength: number | null = null
 
+  /**
+   * The error message displayed when the input value has fewer characters than the specified minimum length.
+   *
+   * @default 'The value must have at least {minLength} characters'
+   */
   @property({ attribute: INPUT_FIELD_ATTR_ERROR_MSG.MinLength })
     errorMessageMinLength = 'The value must have at least {minLength} characters'
 
-  @property({ type: Number })
+  /**
+   * The maximum number of characters allowed in the input field.
+   *
+   * @default null
+   */
+  @property({ type: Number, attribute: 'maxlength' })
     maxLength: number | null = null
 
+  /**
+   * The error message displayed when the input value has more characters than the specified maximum length.
+   *
+   * @default 'The value must have at most {maxLength} characters'
+   */
   @property({ attribute: INPUT_FIELD_ATTR_ERROR_MSG.MaxLength })
     errorMessageMaxLength = 'The value must have at most {maxLength} characters'
 
+  /**
+   * The minimum numerical value allowed in the input field.
+   *
+   * @default null
+   */
   @property({ type: Number })
     min: number | null = null
 
+  /**
+   * The error message displayed when the input value is less than the specified minimum value.
+   *
+   * @default 'The value must be greater than or equal to {min}'
+   */
   @property({ attribute: INPUT_FIELD_ATTR_ERROR_MSG.Min })
     errorMessageMin = 'The value must be greater than or equal to {min}'
 
+  /**
+   * The maximum numerical value allowed in the input field.
+   *
+   * @default null
+   */
   @property({ type: Number })
     max: number | null = null
 
+  /**
+   * The error message displayed when the input value is greater than the specified maximum value.
+   *
+   * @default 'The value must be less than or equal to {max}'
+   */
   @property({ attribute: INPUT_FIELD_ATTR_ERROR_MSG.Max })
     errorMessageMax = 'The value must be less than or equal to {max}'
 
+  /**
+   * A regular expression pattern that the input value must match.
+   *
+   * @default ''
+   */
   @property()
     pattern = ''
 
+  /**
+   * The error message displayed when the input value does not match the specified pattern.
+   *
+   * @default 'The value does not match the pattern'
+   */
   @property({ attribute: INPUT_FIELD_ATTR_ERROR_MSG.Pattern })
     errorMessagePattern = 'The value does not match the pattern'
 
+  /**
+   * The error message displayed when the input value is an invalid email address.
+   *
+   * @default 'The email is invalid'
+   */
   @property({ attribute: INPUT_FIELD_ATTR_ERROR_MSG.Email })
     errorMessageEmail = 'The email is invalid'
 
+  /**
+   * The default error message displayed when the input value is invalid for any other reason not covered by specific rules.
+   *
+   * @default 'The value is invalid'
+   */
   @property({ attribute: INPUT_FIELD_ATTR_ERROR_MSG.Default })
     errorMessageDefault = 'The value is invalid'
 
   /**
    * Indicates whether the input field has been modified.
+   *
+   * @default false
    */
   @property({ attribute: false })
     dirty = false
 
   /**
    * Indicates whether the input field has been touched.
+   *
+   * @default false
    */
   @property({ attribute: false })
     touched = false
@@ -182,6 +248,14 @@ export abstract class InputField<T> extends LitElement {
     }
   }
 
+  /**
+   * Determines if the component has a validation error.
+   * The method checks if there is a validation message and whether the component
+   * has been touched or is dirty.
+   *
+   * @protected
+   * @returns {boolean} `true` if the component has an error, `false` otherwise.
+   */
   protected hasError (): boolean {
     return !!this.validationMessage && (this.touched || this.dirty)
   }
@@ -202,6 +276,8 @@ export abstract class InputField<T> extends LitElement {
 
   /**
    * Return a list of the validation rules registered to the input.
+   *
+   * @returns {RuleEntity[]} The list of validation rules.
    */
   getRules (): RuleEntity[] {
     return [...this._rules].map(([name, handler]) => ({ name, handler }))
@@ -209,6 +285,9 @@ export abstract class InputField<T> extends LitElement {
 
   /**
    * Returns the validation rule with the given name.
+   *
+   * @param name The name of the rule to get.
+   * @returns {RuleHandler | null} The rule handler if it exists, `null` otherwise.
    */
   getRule (name: string): RuleHandler | null {
     const handler = this._rules.get(name)
@@ -222,6 +301,7 @@ export abstract class InputField<T> extends LitElement {
    * Returns true if the input has a validation rule with the given name.
    *
    * @param name The name of the rule to check.
+   * @returns {boolean} `true` if the rule exists, `false` otherwise.
    */
   hasRule (name: string): boolean {
     return this._rules.has(name)
@@ -231,6 +311,7 @@ export abstract class InputField<T> extends LitElement {
    * Adds a validation rule to the input.
    *
    * @param validation The validation rule to add.
+   * @returns {void}
    */
   addRule (validation: RuleEntity): void {
     if (this.hasRule(validation.name)) {
@@ -244,6 +325,7 @@ export abstract class InputField<T> extends LitElement {
    * Removes a validation rule from the input.
    *
    * @param name The name of the rule to remove.
+   * @returns {void}
    */
   removeRule (name: string): void {
     if (!this.hasRule(name)) {
@@ -253,6 +335,18 @@ export abstract class InputField<T> extends LitElement {
     this._rules.delete(name)
   }
 
+  /**
+   * Sets a custom validation message for the component.
+   *
+   * This method allows setting a custom validation message that will be used to
+   * indicate an error state. If a message is provided, it sets the custom validity
+   * and the corresponding validation state. If no message is provided, it clears
+   * the custom validity.
+   *
+   * @param {string} message The custom validation message to set. If empty, the custom
+   * validation state is cleared.
+   * @returns {void}
+   */
   setCustomValidity (message: string): void {
     this._validationMessage = message || null
     this.requestUpdate('_validationMessage')
@@ -276,6 +370,7 @@ export abstract class InputField<T> extends LitElement {
    * Sets a custom validation message.
    *
    * @param message The validation message to set.
+   * @returns {void}
    * @deprecated
    */
   setValidity (message: string | null): void {
@@ -299,10 +394,16 @@ export abstract class InputField<T> extends LitElement {
   }
 
   /**
-   * Configures the validation rules for the input element.
-   * Parses the rules from the component's 'rules' property,
-   * sets corresponding attributes on the native input element,
-   * and associates validators based on the defined rules.
+   * Configures the validation rules for the component.
+   *
+   * This method parses the validation rules provided in the `rules` property
+   * to set the corresponding properties such as `required`, `minLength`,
+   * `maxLength`, `min`, `max`, and `pattern`. This implementation ensures
+   * backward compatibility with the previous method of defining rules via
+   * a string (e.g., `required|minlength:3|maxlength:10`).
+   *
+   * @private
+   * @returns {void}
    */
   private _configureRules (): void {
     const parsingRules = createParsingRules(this.rules)
@@ -328,6 +429,9 @@ export abstract class InputField<T> extends LitElement {
   /**
    * This method is responsible for validating all the rules
    * associated with the input element.
+   *
+   * @param data The data to validate.
+   * @returns {Promise<Feedback | null>} A promise that resolves to a feedback object if the validation fails, `null` otherwise.
    */
   protected async validate (data: ValidationContext): Promise<Feedback | null> {
     const rules = this.getRules()
@@ -355,6 +459,7 @@ export abstract class InputField<T> extends LitElement {
   /**
    * This method is responsible for triggering the validation.
    *
+   * @returns {Promise<void>}
    * @deprecated
    */
   protected async triggerValidation (): Promise<void> {
@@ -379,6 +484,8 @@ export abstract class InputField<T> extends LitElement {
 
   /**
    * Reset the state of the input field.
+   *
+   * @returns {void}
    */
   abstract reset(): void
 
